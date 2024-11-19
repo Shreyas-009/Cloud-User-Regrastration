d52bf79e5192b17f2ea2353d8cb0fba4f619efa4
Shreyas <shreyastungar762@gmail.com>
2024-11-19 18:51:31 +0530

    url changed

--- frontend/src/utils/usl.jsx
+++ frontend/src/utils/usl.jsx
@@ -1,3 +1,3 @@
 // const api = "http://localhost:8080";
-const api = "https://cloud-user-regrastrationbackend.vercel.app/";
+const api = "https://cloud-user-regrastrationbackend.vercel.app";
 export default api;
