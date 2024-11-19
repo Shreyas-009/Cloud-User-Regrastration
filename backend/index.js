e47ba02c07a8825d51d4b6afca859ef21d2d3f1f
Shreyas <shreyastungar762@gmail.com>
2024-11-19 18:58:12 +0530

    cors error resolved

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ const User = mongoose.model("User", userSchema);
 // Middleware
 app.use(
   cors({
-    origin: "https://cloud-user-regrastration.vercel.app/",
+    origin: "*",
     credentials: true,
   })
 );
